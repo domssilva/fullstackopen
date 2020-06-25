66d69dad48f046f31b29eb442cbd318c18d8799a
domssilva <silva@programmer.net>
2020-06-25 11:14:21 +0200

    [part1] - assignment 1.2

--- part1/courseinfo/src/Components/Content.jsx
+++ part1/courseinfo/src/Components/Content.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 
+import Part from './Part'
+
 const Content = props => (
-  <>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
-  </>
+  <div>
+    <Part part={props.part1} exercises={props.exercises1}/>
+    <Part part={props.part2} exercises={props.exercises2}/>
+    <Part part={props.part3} exercises={props.exercises3}/>
+  </div>
 )
 
 export default Content
---
+++ part1/courseinfo/src/Components/Part.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Part = props => (
+  <p>
+    {props.part} {props.exercises}
+  </p>
+) 
+
+export default Part
