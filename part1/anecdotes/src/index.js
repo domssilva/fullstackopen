60414d72e6897bc3bbfa147631a7d5f181458fba
domssilva <silva@programmer.net>
2020-06-25 16:56:06 +0200

    [part1] - assignment 1.13

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -5,22 +5,56 @@ import './index.css';
 
 const App = props => {
   const [selected, setSelected] = useState(0)
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      text: 'If it hurts, do it more often',
+      votes: 0,
+    },
+    {
+      text: 'Adding manpower to a late software project makes it later!',
+      votes: 0,
+    },
+    {
+      text: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+      votes: 0,
+    },
+    {
+      text: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+      votes: 0,
+    },
+    {
+      text: 'Premature optimization is the root of all evil.',
+      votes: 0,
+    },
+    {
+      text: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+      votes: 0,
+    },
+  ])
 
   const getRandomNumber = () => {
     const min = 0
-    const max = props.anecdotes.length - 1
+    const max = anecdotes.length - 1
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
   const nextAnecdote = () => setSelected(getRandomNumber())
+
+  const voteAnecdote = () => {
+    let newArray = [...anecdotes]
+    newArray[selected].votes += 1
+    setAnecdotes(newArray)
+  }
 
   return (
     <div>
       <p style={{
         minHeight: '85px',
       }}>
-        {props.anecdotes[selected]}
+        {anecdotes[selected].text}
       </p>
+      <p>has {anecdotes[selected].votes} votes</p>
+      <button onClick={voteAnecdote}>vote</button>
       <button 
         onClick={nextAnecdote}>
         next anecdote
@@ -28,17 +62,8 @@ const App = props => {
     </div>
   )
 }
-  
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
 
 ReactDOM.render(
-  <App anecdotes={anecdotes}/>,
+  <App/>,
   document.getElementById('root')
 )
