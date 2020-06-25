b0292279603cb61658e5fb5999cb186e4fe96a39
domssilva <silva@programmer.net>
2020-06-25 11:08:03 +0200

    [part1] - assignment 1.1

---
+++ part1/courseinfo/src/Components/Total.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Total = props => {
+  return (
+    (
+      <p>Number of exercises {props.total}</p>
+    )
+  )
+}
+
+export default Total
---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './Components/Header'
+import Content from './Components/Content'
+import Total from './Components/Total'
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content 
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
