6f50c5e3ab515fa9d146a64ab9fbb3d1ef6fd7e5
domssilva <silva@programmer.net>
2020-06-27 22:30:01 +0200

    [part2] - assignment 2.4

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -4,33 +4,58 @@ import ReactDOM from 'react-dom';
 import Course from './Components/Course'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4,
-      },
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course}/>
+  return (
+    <>
+      <h1>Web Development Curriculum</h1>
+      <Course course={courses[0]}/>
+      <Course course={courses[1]}/>
+    </>
+  )
 }
 
 ReactDOM.render(<App/>, document.getElementById('root'))
