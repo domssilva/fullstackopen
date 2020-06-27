b5d71519d37b547dce6e7dac3453f15fdab51e11
domssilva <silva@programmer.net>
2020-06-27 22:27:14 +0200

    [part2] - assignment 2.3

--- part2/courseinfo/src/Components/Total.jsx
+++ part2/courseinfo/src/Components/Total.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
-const Total = props => {
-  let total = 0;
-  const sum = props.parts.map(obj => total += obj.exercises)
+const Total = ({parts}) => {
+  const total = parts.reduce((total, amount) => total + Number(amount.exercises), 0)
 
   return (
     (
