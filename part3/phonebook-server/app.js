bcca3b798d798516c25e774d116df55632e57b7a
domssilva <silva@programmer.net>
2020-08-04 14:17:00 +0200

    [part3] - assignment 3.1

---
+++ part3/phonebook-server/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const data = [
+      {
+        "name": "Hermione Granger",
+        "number": "39-07592385",
+        "id": 1
+      },
+      {
+        "name": "Ron Wisley",
+        "number": "89-34589236",
+        "id": 2
+      },
+      {
+        "name": "Harry Potter",
+        "number": "829-1095209",
+        "id": 3
+      },
+      {
+        "name": "Severus Snape",
+        "number": "81-81095392",
+        "id": 4
+      }
+    ];
+
+app.get('/api/persons', (req, res) => {
+    res.send(data);
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
