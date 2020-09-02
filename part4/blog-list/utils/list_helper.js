bf1be10c3870261ae351a8a242c7f0d2afd2265a
domssilva <silva@programmer.net>
2020-09-02 21:19:30 +0200

    [part4] - assignment 4.4

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,32 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogPosts) => {
+  let likes = 0
+  for (let i = 0; i < blogPosts.length; i++) {
+    likes += blogPosts[i].likes
+  }
+
+  return likes
+}
+
+const favoriteBlog = (blogs) => {
+  // pick the most liked blog post from an array
+  let fave = 0
+  let blog = {}
+
+  blogs.forEach(post => {
+    if (post.likes > fave) {
+      fave = post.likes
+      blog = post
+    }
+  })
+
+  return blog
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog,
 }
