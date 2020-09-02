1435c817710eb0a2b0fb5f6f3440a1d893f561a3
domssilva <silva@programmer.net>
2020-09-02 13:39:24 +0200

    [part4] - project structure: config

---
+++ part4/notes-server/index.js
@@ -0,0 +1,99 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const Note = require('./models/note')
+const { response } = require('express')
+
+const PORT = process.env.PORT
+
+const requestLogger = (request, response, next) => {
+  console.log('--------------------')
+  console.log(`${request.method} ${request.path}`)
+  console.log('Body: ', request.body)
+  console.log('--------------------')
+  next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({
+      error: error.message,
+    })
+  }
+
+  next(error)
+}
+
+// middlewares
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(requestLogger)
+app.use(errorHandler)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+///////////////////////////////////////////
+// ROUTES
+
+// get all notes
+app.get('/api/notes', (req, res) => {
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
+})
+
+// get single note
+app.get('/api/notes/:id', (req, res, next) => {
+  Note
+    .findById(req.params.id)
+    .then(note => {
+      if (note) {
+        res.json(note)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+// remove note
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+// create note
+app.post('/api/notes', (req, res, next) => {
+  const body = req.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  note.save()
+    .then(savedNote => savedNote.toJSON())
+    .then(savedAndFormattedNote => res.json(savedAndFormattedNote))
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+
+// start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ part4/notes-server/models/note.js
@@ -0,0 +1,33 @@
+const { MONGO_URI } = require('../utils/config')
+const mongoose = require('mongoose')
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minlength: 5,
+    required: true,
+  },
+  date: Date,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
