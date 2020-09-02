9c368ace3e9e9ca92040e0d712d0342052ac58f6
domssilva <silva@programmer.net>
2020-09-02 14:12:04 +0200

    [part4] - project structure: middleware

--- part4/notes-server/index.js
+++ part4/notes-server/index.js
@@ -5,14 +5,18 @@ const app = express()
 const cors = require('cors')
 const logger = require('./utils/logger')
 const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
 const { PORT } = require('./utils/config')
 
-// middlewares
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 // start server
 app.listen(PORT, () => {
