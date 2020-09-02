9df0b8fdca26c3018fcca6636aa49c8ca3e448e5
domssilva <silva@programmer.net>
2020-09-02 14:15:25 +0200

    [part4] - project structure: separate app and index

--- part4/notes-server/index.js
+++ part4/notes-server/index.js
@@ -1,24 +1,10 @@
-require('dotenv').config()
-
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app') // the actual Express application
+const http = require('http')
+const config = require('./utils/config')
 const logger = require('./utils/logger')
-const notesRouter = require('./controllers/notes')
-const middleware = require('./utils/middleware')
-const { PORT } = require('./utils/config')
 
-app.use(cors())
-app.use(express.static('build'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+const server = http.createServer(app)
 
-app.use('/api/notes', notesRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-// start server
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
